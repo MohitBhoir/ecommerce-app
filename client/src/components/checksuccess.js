91066ef7e3f38f1a6cdd642ed4caf756653c9eb8
MohitBhoir <114341042+MohitBhoir@users.noreply.github.com>
2023-07-24 08:08:38 +0530

    some more ui

--- client/src/components/checksuccess.js
+++ client/src/components/checksuccess.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const CheckSuccess = () => {
-  return (
-    <div>CheckSuccess</div>
-  )
+  return <>
+     <div className='flex-col p-4'>
+          <h1 className='mx-[45%] text-3xl font-bold text-green-600'>payment successful</h1>
+         <img src={require('./success.jpg')} alt=""  className='h-[300px] w-[300px] mx-[40%] my-8
+         rounded-full animate-pulse'/>
+     </div>
+  </>
 }
 
 export default CheckSuccess
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import Main from '../components/main'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return <>
       <Main/>
-      <section className='border-[2px] border-black w-max m-2 p-2'>
-        <h1 className='p-2 text-2xl font-bold'>Up to 50% off on electroincs</h1>
-        <div className='flex'>
-            <div>
+      <div className='flex'>
+           <section className='border-[2px] bg-[#37475A]  w-max m-2 p-2'>
+        <h1 className='p-2 text-2xl font-bold text-white'>Up to 50% off on electroincs</h1>
+        <div>
+            <div className='flex gap-2 mb-4'>
                 <div><img src={require('./wash1.jpg')} alt="" /></div>
                 <div><img src={require('./wash2.jpg')} alt="" /></div>
             </div>
-            <div>
+            <div className='flex gap-2'>
                 <div><img src={require('./mobile1.jpg')} alt="" /></div>
                 <div><img src={require('./mobile2.jpg')} alt="" /></div>
             </div>
         </div>
       </section>
+       <section className='border-[2px] bg-[#f0c14b] w-max m-2 p-2'>
+        <h1 className='p-2 text-2xl font-bold'>Up to 60% off | styles for men</h1>
+        <div className='flex gap-4'>
+                <div><img src={require('./watch.jpg')} alt=""/></div>
+                <div><img src={require('./shoes.jpg')} alt="" /></div>
+        </div>
+        <Link to='/shop'>explore more</Link>
+      </section>
+      </div>
   </>
 }
 
